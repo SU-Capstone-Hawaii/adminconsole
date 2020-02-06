2f2322d94d93c1fb5b7d905bb0be80ad6f3e6bb1
Jake Ladera <laderah@seattleu.edu>
2020-02-05 22:47:30 -0800

    Added comments and removed unnecessary directives

--- adminconsole/Models/Locations.cs
+++ adminconsole/Models/Locations.cs
@@ -1,65 +1,198 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace adminconsole.Models
 {
     public partial class Locations
     {
+        /// <summary>
+        /// LocationId
+        /// </summary>
         [Required]
         public string LocationId { get; set; }
 
+
+
+
+        /// <summary>
+        /// Co-Op's LocationId 
+        /// </summary>
         public string CoopLocationId { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// Indicates if location record gets updated with every 
+        /// batch execution
+        /// </summary>
         [DisplayName("Take Co-Op Data")]
         public bool? TakeCoopData { get; set; }
 
 
+
+
+
+        /// <summary>
+        /// Indicates not to show to external members, but can
+        /// be viewed by System Admins. Also allows System Admin
+        /// to recover the location record.
+        /// </summary>
         public bool? SoftDelete { get; set; }
 
+
+
+
+
+
+        /// <summary>
+        /// Institution Name
+        /// </summary>
         [DisplayName("Institution")]
         public string Name { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// Street address
+        /// </summary>
         [Required]
         [DisplayName("Address")]
         public string Address { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// City Name
+        /// </summary>
         [Required]
         [DisplayName("City")]
         public string City { get; set; }
 
+
+
+
+
+
+        /// <summary>
+        /// County name (if provided)
+        /// </summary>
         [DisplayName("County")]
         public string County { get; set; }
 
+
+
+
+
+
+        /// <summary>
+        /// 2 letter State code
+        /// </summary>
         [Required]
         [DisplayName("State")]
         public string State { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// Postal code
+        /// </summary>
         [Required]
         [DisplayName("Zipcode")]
         public string PostalCode { get; set; }
 
+
+
+
+
+
+
+        /// <summary>
+        /// 2 letter Country code
+        /// 
+        /// Should be blank or US for now
+        /// </summary>
         [DisplayName("Country")]
         public string Country { get; set; }
 
+
+
+
+
+
+
+        /// <summary>
+        /// Latitude
+        /// </summary>
         [Required]
         [DisplayName("Latitude")]
         public decimal Latitude { get; set; }
 
+
+
+
+
+
+
+
+        /// <summary>
+        /// Longitude
+        /// </summary>
         [Required]
         [DisplayName("Longitude")]
         public decimal Longitude { get; set; }
 
+
+
+
+
+
+
+        /// <summary>
+        /// Provided by Co-Op. 
+        /// 
+        /// Is either 24rs Access or Business Hours Access
+        /// </summary>
         [DisplayName("24hrs / Business Hours Access")]
         public string Hours { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// Retail Outlet name
+        /// 
+        /// ex. Safeway or _________ Mall
+        /// </summary>
         [DisplayName("Retail Outlet")]
         public string RetailOutlet { get; set; }
 
+
+
+
+
+
+        /// <summary>
+        /// Location type.
+        /// 
+        /// Either A (ATM) or S (Shared Branch)
+        /// </summary>
         [Required]
         [DisplayName("Location Type")]
         public string LocationType { get; set; }
+
+
+
+
+
 
 
         #region Objects Used for Joins
