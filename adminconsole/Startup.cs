60666d92bff7a97200305c1a950c01407d84247b
Jake Ladera <laderah@seattleu.edu>
2020-02-06 15:41:14 -0800

    Added Login Logout

--- adminconsole/Startup.cs
+++ adminconsole/Startup.cs
@@ -49,7 +49,7 @@ namespace adminconsole
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Locations}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
