67294009d46a5a1d280f4fc64872780353e93469
Jake Ladera <laderah@seattleu.edu>
2020-02-08 12:33:08 -0800

    Interface to allow for polymorphism

---
+++ adminconsole/Models/IMaphawksDatabaseTable.cs
@@ -0,0 +1,35 @@
+﻿namespace adminconsole.Models
+{
+    
+    public interface IMaphawksDatabaseTable 
+    {
+
+        //public Tables 
+
+        /// <summary>
+        /// Will be used to determine if all properties are null in Table Classes
+        /// </summary>
+        /// 
+        /// 
+        /// <returns> True if all are null, otherwise False </returns>
+        public abstract bool AllPropertiesAreNull();
+
+
+        
+  
+    }
+}
+
+
+public enum Table
+{
+    none = 0,
+
+    location = 1,
+
+    contact = 2,
+
+    special_qualities = 3,
+
+    daily_hours = 4
+}
