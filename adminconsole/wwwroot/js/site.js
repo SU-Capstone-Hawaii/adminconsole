5abe21c68b37012de1e59f330d515fe89ca55d89
Jake Ladera <laderah@seattleu.edu>
2020-02-11 13:58:49 -0800

    Added test map (need to resize)

--- adminconsole/wwwroot/js/site.js
+++ adminconsole/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function initMap() {
+    var center = { lat: 55.92965249, lng: 12.47840507 };
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: center
+    });
+}
