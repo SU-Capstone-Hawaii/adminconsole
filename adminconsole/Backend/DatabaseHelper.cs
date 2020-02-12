e1f9f7d0d303a3ddc0b49bad539c4ecec2217db0
Jake Ladera <laderah@seattleu.edu>
2020-02-12 00:39:56 -0800

    Added Database helper class for backend
    Will help with mocking for unit testing

---
+++ adminconsole/Backend/DatabaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using adminconsole.Models;
+
+
+namespace adminconsole.Backend
+{
+    public class DatabaseHelper
+    {
+
+        private MaphawksContext _context;
+
+        public DatabaseHelper(MaphawksContext c)
+        {
+            _context = c;
+        }
+    }
+}
