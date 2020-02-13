6f24a997c22c33a23a51ddff4c8b3d55e12e5b14
Jake Ladera <laderah@seattleu.edu>
2020-02-12 17:51:26 -0800

    Adds draggable google map with custom marker

--- adminconsole/wwwroot/js/site.js
+++ adminconsole/wwwroot/js/site.js
@@ -2,47 +2,88 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function initMap() {
-    // set BECU Headquarters for default map starting location
-    /*var headquarters = {
-        lat: 47.490305,
-        lng: -122.272081
-    }*/
 
-    // The location of Uluru
-    var uluru = { lat: 47.490305, lng: -122.272081};
-    // The map, centered at Uluru
+
+
+/**
+ * Itinitalizes Google Map drawing with marker. This function is called on load
+ * of Edit and Create views
+ */
+function initMap() {
+
+    var lat = parseFloat($('#Latitude').attr('value'));
+    var long = parseFloat($('#Longitude').attr('value'))
+
+
+
+    if ((lat != null) && (long != null)) { // If we have a latitude and longitude, display map and drop pin
+        displayMap(lat, long);
+    }
+
+}
+
+
+
+/**
+ * Displays the Google Map and a marker on the page
+ * @param {any} lat:    Location Latitude
+ * @param {any} long:   Location Longitude
+ */
+function displayMap(lat, long) {
+
+
+    var location = { lat: lat, lng: long };
+
+
+
+
+    // Center map on location
     var map = new google.maps.Map(
         document.getElementById('map-wrapper'), {
-            zoom: 15,
-            center: uluru
+        zoom: 15,
+        center: location
     });
 
+
+
+
+    // Generate pin image to indicate position on map
     var image = {
         url: '/media/pin.png',
         scaledSize: new google.maps.Size(40, 50),
     };
-    // The marker, positioned at Uluru
+
+
+
+    // Position marker on map
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: location,
         map: map,
-        icon: image,
         draggable: true
     });
-    //var marker = new google.maps.Marker({ position: uluru, map: map });
-
-    // *** set default map parameters *** //  
-    /*var mapOptions = {
-        center: headquarters,
-        gestureHandling: 'cooperative',
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        //streetViewControl: false,
-        zoom: 15,
-    };
-
-    // ***  display the Google map with the default Options  *** //
-    map = new google.maps.Map(document.getElementById('map-wrapper'), mapOptions);*/
 
 
+
+    // Change value of Latitude and Longitude based on marker position
+    google.maps.event.addListener(marker, 'dragend', function (marker) {
+        var latLng = marker.latLng;
+        var lat = latLng.lat().toFixedDown(6);
+        var long = latLng.lng().toFixedDown(6);
+        $('#Latitude').val(lat);
+        $('#Longitude').val(long);
+    });
+
+}
+
+
+
+/**
+ * Truncates JavaScript Float to maximum of 6 digits after the decimal. Left as 
+ * a parameter for long-term code flexibility.
+ */
+Number.prototype.toFixedDown = function (digits = 6) {
+    var re = new RegExp("([-+]?\\d+\\.\\d{" + digits + "})(\\d)"),
+        m = this.toString().match(re);
+    return m ? parseFloat(m[1]) : this.valueOf();
 };
 
