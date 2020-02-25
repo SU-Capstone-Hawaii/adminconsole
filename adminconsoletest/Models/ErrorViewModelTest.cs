5b7cfe63f27867bba23d229586478640a891750a
Jake Ladera <laderah@seattleu.edu>
2020-02-24 17:25:56 -0800

    Test ErrorViewModel properties set

--- adminconsoletest/Models/ErrorViewModelTest.cs
+++ adminconsoletest/Models/ErrorViewModelTest.cs
@@ -6,6 +6,8 @@ namespace adminconsoletest
     [TestClass]
     public class ErrorViewModelTest
     {
+
+        // Ensures default values of Class instantiation are intact
         [TestMethod]
         public void ErrorViewModelTest_Default_Should_Pass()
         {
@@ -17,6 +19,25 @@ namespace adminconsoletest
             // Assert
             Assert.IsNull(errorViewModel.RequestId);
             Assert.IsFalse(errorViewModel.ShowRequestId);
+        }
+
+
+
+        // Ensures Set of class variables are intact
+        [TestMethod]
+        public void ErrorViewModelTest_Set_RequestId_Should_Pass()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel();
+            var expected = "Error";
+
+            // Act
+            errorViewModel.RequestId = expected;
+
+            // Assert
+            Assert.IsNotNull(errorViewModel.RequestId);
+            Assert.AreEqual(expected, errorViewModel.RequestId);
+            Assert.IsTrue(errorViewModel.ShowRequestId);
         }
     }
 }
