167f066ee8b11766d9192c8635f9a57f2feea10f
Jake Ladera <laderah@seattleu.edu>
2020-02-25 14:14:29 -0800

    Test DataMock GetOneLocation Null Param

--- adminconsoletest/MockData/LocationsDataMockTest.cs
+++ adminconsoletest/MockData/LocationsDataMockTest.cs
@@ -6,6 +6,8 @@ namespace adminconsoletest
     [TestClass]
     public class LocationsDataMockTest
     {
+
+        // Tests Constructor
         [TestMethod]
         public void LocationsDataMock_Constructor_Should_Pass()
         {
@@ -19,6 +21,28 @@ namespace adminconsoletest
 
             // Assert
             Assert.IsNotNull(dataMock);
+        }
+
+
+
+
+
+
+        // Tests null parameter for GetOneLocation
+        [TestMethod]
+        public void LocationsDataMock_GetOneLocation_WhereClause_Param_Is_Null_Should_Pass()
+        {
+            // Arrange
+            var dataMock = new LocationsDataMock();
+
+
+            // Act
+            var result = dataMock.GetOneLocation(null);
+
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.LocationId, "a91be80e-ed05-4157-bb95-aa3494663d2a");
         }
     }
 }
